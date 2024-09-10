bd865d34f431947f3b3d25546210698bf1b6d9b8
Lucas S. Santos <lcsantos.xp@gmail.com>
2024-09-10 11:20:34 -0300

    Alterações pendentes
    Valeu a tentativa...

--- dados.js
+++ dados.js
@@ -1,28 +1,27 @@
 let dados = [
     {
         titulo: "Programação",
-        descrição: "Nosso software pare ajudar usuarios com questões relacionadas ao vício digital",
+        descricao: "Nosso software pare ajudar usuarios com questões relacionadas ao vício digital",
         participar: "Download"
 
     },
 
     {
         titulo: "Terapia online",
-        descrição: "Oferecemos terapia online para auxiliar no tratamento do vício digital.",
+        descricao: "Oferecemos terapia online para auxiliar no tratamento do vício digital.",
         participar: "Inscreva-se"
     },
 
     {
         titulo: "Comunidade de apoio",
-        descrição: "Junte-se à nossa comunidade para compartilhar experiências e encontrar apoio.",
+        descricao: "Junte-se à nossa comunidade para compartilhar experiências e encontrar apoio.",
         participar: "Inscrições abertas"
     },
 
     {
         titulo: "Dicas e ferramentas",
-        descrição: "Acesse nossas dicas e ferramentas para controlar o uso da tecnologia.",
+        descricao: "Acesse nossas dicas e ferramentas para controlar o uso da tecnologia.",
         participar: "Atualizado mensalmente"
     }
 
-
-]
+];
